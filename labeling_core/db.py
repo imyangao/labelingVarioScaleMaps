1f801ad5a6afd781eb9b82271cbd1b3caba464b4
Yan <luckygao2023@163.com>
2025-06-18 00:14:07 +0200

    remove psw

--- labeling_core/db.py
+++ labeling_core/db.py
@@ -5,9 +5,9 @@ from urllib.parse import quote_plus
 ##############################################################################
 # Database connection
 ##############################################################################
-DB_NAME = "tgap_test"
-DB_USER = "postgres"
-DB_PASS = "Gy@001130"
+DB_NAME = ""
+DB_USER = ""
+DB_PASS = ""
 DB_HOST = "localhost"
 DB_PORT = 5432
 
@@ -20,7 +20,6 @@ BULD_MIN, BULD_MAX = 13000, 14000
 
 
 def get_connection(autocommit=True):
-    """Establishes and returns a psycopg2 database connection."""
     conn = psycopg2.connect(
         dbname=DB_NAME,
         user=DB_USER,
@@ -32,7 +31,6 @@ def get_connection(autocommit=True):
     return conn
 
 def get_engine():
-    """Creates and returns a SQLAlchemy engine."""
     escaped_password = quote_plus(DB_PASS)
     db_uri = f"postgresql://{DB_USER}:{escaped_password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     return create_engine(db_uri) 
